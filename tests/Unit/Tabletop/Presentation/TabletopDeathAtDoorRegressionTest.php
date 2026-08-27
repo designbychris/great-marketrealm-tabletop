b9aacadd947a9f351b0766728ae0560b2b21a763
Christopher Mitchell <chris@designbychris.co.uk>
2026-08-27 10:03:46 +0100

    Update TabletopDeathAtDoorRegressionTest.php

--- tests/Unit/Tabletop/Presentation/TabletopDeathAtDoorRegressionTest.php
+++ tests/Unit/Tabletop/Presentation/TabletopDeathAtDoorRegressionTest.php
@@ -10,12 +10,25 @@ final class TabletopDeathAtDoorRegressionTest extends TestCase
 {
     public function testCombatHudDistinguishesFallenAndStable(): void
     {
-        $source = (string) file_get_contents(
-            dirname(__DIR__, 4)
-                . '/app/Tabletop/Views/chamber.php'
+        $view = file_get_contents(
+            dirname(__DIR__, 4) . '/app/Views/tabletop/chamber.php'
         );
-
-        self::assertStringContainsString('>Fallen<', $source);
-        self::assertStringContainsString('>Stable<', $source);
+    
+        self::assertIsString($view);
+    
+        self::assertStringContainsString(
+            "'DECEASED' : 'DOWN'",
+            $view
+        );
+    
+        self::assertStringContainsString(
+            '>Death confirmed<',
+            $view
+        );
+    
+        self::assertStringContainsString(
+            '>Stable<',
+            $view
+        );
     }
 }
