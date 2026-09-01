72c86e1eb86ed57f559f6cb081f1102a9b274afe
Christopher Mitchell <chris@designbychris.co.uk>
2026-09-01 10:12:18 +0100

    Add files via upload

--- tests/Unit/Architecture/PluginFoundationTest.php
+++ tests/Unit/Architecture/PluginFoundationTest.php
@@ -24,7 +24,7 @@ final class PluginFoundationTest extends TestCase
             $source
         );
         self::assertStringContainsString(
-            "define('GMRT_VERSION', '0.30.1-alpha.13')",
+            "define('GMRT_VERSION', '0.31.0-alpha.1')",
             $source
         );
         self::assertStringContainsString(
